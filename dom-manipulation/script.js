26fe4ce43fd1f1eb2a68b4c3875d49ba5690a3b7
ahmed elhadidy <ahmedtamerelhadidy1@gmail.com>
2024-07-25 17:48:42 +0300

    first commit

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -106,7 +106,7 @@ async function fetchQuotesFromServer() {
       const serverQuotes = await response.json();
       const updatedQuotes = serverQuotes.map(post => ({ text: post.title, category: 'Server' }));
 
-      // Merge server and local quotes, prioritizing server data
+      // ["syncQuotes"]g
       quotes = [...updatedQuotes, ...quotes.filter(quote => !updatedQuotes.some(sq => sq.text === quote.text))];
       saveQuotes();
       populateCategories();
